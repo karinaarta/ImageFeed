31e4d2140ff7c7d332f41fa4f85a5290fc18027b
Artamonova Karina <pakarin@yandex.ru>
2024-10-06 08:35:26 +0300

    Создана верстка ланчскрина и таблица

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by karine pankova on 27.09.2024.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    @IBOutlet private var tableView: UITableView!
+    
+}
+
+extension ImagesListViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    
+}
+
+extension ImagesListViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) // 1
+        
+        guard let imageListCell = cell as? ImagesListCell else { // 2
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell, with: indexPath) // 3
+        return imageListCell // 4
+    }
+}
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+        cell.cellImage.layer.cornerRadius = 16
+        cell.cellImage.layer.masksToBounds = true
+        cell.cellImage.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "like_on") : UIImage(named: "like_off")
+        cell.likeButton.setImage(likeImage, for: .normal)
+    }
+}
+
