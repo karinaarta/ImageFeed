6ed9b15f968ddf66129140de52d0931b228dccc1
Artamonova Karina <pakarin@yandex.ru>
2024-10-08 09:59:27 +0300

    Добален новый экран, навигешн и тэб бар

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by karine pankova on 07.10.2024.
+//
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func didTapButton() {
+    }
+}
+
