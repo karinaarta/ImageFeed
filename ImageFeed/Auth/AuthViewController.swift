8f93a7780d66bcd30e27791466a992d7e72275b5
Artamonova Karina <pakarin@yandex.ru>
2024-11-01 09:39:27 +0300

    добавлен splash контроллер

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -6,26 +6,33 @@
 //
 
 import UIKit
+protocol AuthViewControllerDelegate: AnyObject {
+    func didAuthenticate(_ vc: AuthViewController)
+}
+
 
 final class AuthViewController: UIViewController {
     private let ShowWebViewSegueIdentifier = "ShowWebView"
     let auth2Service = OAuth2Service()
     let tokenStorage = OAuth2TokenStorage()
+    weak var delegate: AuthViewControllerDelegate?
 }
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        vc.dismiss(animated: true)
         auth2Service.fetchOAuthToken(code: code, completion: {result in
             switch result{
             case.success(let responceBody):
                 self.tokenStorage.token = responceBody.accessToken
+                self.delegate?.didAuthenticate(self)
             case.failure(let error):
                 print (error.localizedDescription)
             }
         })
     }
-
+    
     func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
         dismiss(animated: true)
     }
-
+    
 }
